f4153026a4b537906633ae5e1ab8bb054980d602
Michael Chung <mchung@pocketgems.com>
2018-07-18 10:58:22 -0700

    Modified comments in Component.java to clarify the types of methods that may be defined in a component

--- src/pocketgems/mud/components/Component.java
+++ src/pocketgems/mud/components/Component.java
@@ -3,8 +3,8 @@ package pocketgems.mud.components;
 /*
  * Component
  * =========
- * A component contains data, and is owned by an Entity. Components should define state, and avoid
- * defining behaviors such as game logic.
+ * A component contains data, and is owned by an Entity. Components should primarily define state, and
+ * may include some methods to help manage the state. However, avoid putting game logic into components.
  */
 public abstract class Component {
 
